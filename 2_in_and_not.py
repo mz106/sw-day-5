17b56794fcf26e307cd146cb196e2c1a3f266acd
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2025-06-26 14:56:54 +0100

    add in operator code

---
+++ 2_in_and_not.py
@@ -0,0 +1,20 @@
+
+
+
+# A coat of paint
+# a coat of paint
+# A Coat Of Paint
+# a coat of PAINT
+
+# coat of paint
+# coats of paints
+# coats paint
+# paint coats
+
+shopping_list = input('Is milk on the list: ').lower()
+
+
+if 'milk' in shopping_list:
+    print('Milk is on the list')
+else: 
+    print('Milk is missing')
