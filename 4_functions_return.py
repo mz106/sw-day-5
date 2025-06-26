1452426c88840976ee69a28a426e3a89697e5f34
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2025-06-26 16:31:31 +0100

    add function code

---
+++ 4_functions_return.py
@@ -0,0 +1,35 @@
+
+
+
+
+# def coffee_machine(coffee_size, cup, type_coffee):
+#     coffee_desc = f'Coffee Size: {coffee_size}, Cup: {cup}, Type of Coffee: {type_coffee}'
+#     return coffee_desc
+
+
+# my_coffee_order = coffee_machine('large', 'red', 'nescafe')
+# print(my_coffee_order)
+
+def add_up(x, y):
+    result = x + y
+    return result
+
+answer_1 = add_up(5, 2)
+print(answer_1)
+
+answer_2 = add_up(3, 5)
+print(answer_2)
+
+def subtract(x, y):
+    result = x - y
+    return result
+
+answer_3 = subtract(10, 5)
+print(answer_3)
+
+def weird_function(thing):
+    # print(f'Hello {thing}')
+    result = 'result'
+
+print(weird_function('cheese'))
+
